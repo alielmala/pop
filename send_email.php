419d5dc5151be5937d3614dccd4ac02483174602
alielmala <ali.elmala22@gmail.com>
2023-05-18 00:42:36 +0300

    initial

---
+++ send_email.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Get the form data
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  // Set the recipient email address
+  $to = 'ali.elmala007@gmail.com';
+
+  // Set the email subject
+  $subject = 'New Form Submission';
+
+  // Set the email headers
+  $headers = "From: $name <$email>\r\n";
+  $headers .= "Reply-To: $email\r\n";
+
+  // Build the email body
+  $body = "Name: $name\n";
+  $body .= "Email: $email\n\n";
+
+  // Send the email
+  if (mail($to, $subject, $body, $headers)) {
+        <script type="text/javascript">
+// JavaScript code
+function myFunction() {
+  echo 'success.';
+  } else {
+    echo 'Error: Unable to send email.';
+  }
+}
+?>
